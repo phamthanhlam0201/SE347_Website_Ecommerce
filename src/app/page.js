bdadb543222a9ddbc2e31b64380366eda142c752
phamthanhlam0201 <phamthanhlam02012003@gmail.com>
2023-12-18 15:03:21 +0700

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,154 @@
+"use client";
+
+import { GlobalContext } from "@/context";
+import { getAllAdminProducts } from "@/services/product";
+import { useRouter } from "next/navigation";
+import { useContext, useEffect, useState } from "react";
+
+export default function Home() {
+  const { isAuthUser } = useContext(GlobalContext);
+
+  const [products, setProducts] = useState([]);
+  const router = useRouter();
+
+  async function getListOfProducts() {
+    const res = await getAllAdminProducts();
+
+    if (res.success) {
+      setProducts(res.data);
+    }
+  }
+
+  useEffect(() => {
+    getListOfProducts();
+  }, []);
+
+  console.log(products);
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <section className="">
+        <div className="grid max-w-screen-xl px-4 py-8 mx-suto  lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+          <div className="mr-auto place-self-center lg:col-span-7">
+            <h2 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-4xl xl:text-5xl">
+              Spring Sale Collection
+            </h2>
+            <p className="max-w-2xl mb-6 font-semibold text-lime-500 lg:mb-8 md:text-lg lg:text-xl">
+              Chào đón Xuân Giáp Thìn 2024 !
+            </p>
+
+            <button
+              type="button"
+              onClick={() => router.push("/product/listing/all-products")}
+              className="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white"
+            >
+              Go to STORE
+            </button>
+            <div className="lg:col-span-2 lg:py-8">
+              <ul className="grid grid-cols-2 gap-4">
+                {products && products.length
+                  ? products
+                      .filter((item) => item.onSale === "yes")
+                      .splice(0, 2)
+                      .map((productItem) => (
+                        <li
+                          onClick={() =>
+                            router.push(`/product/${productItem._id}`)
+                          }
+                          className="cursor-pointer"
+                          key={productItem._id}
+                        >
+                          <div>
+                            <img
+                              src={productItem.imageUrl}
+                              alt="Sale Product Item"
+                              className="object-cover w-full rounded aspect-square"
+                            />
+                          </div>
+                          <div className="mt-3">
+                            <h3 className="font-medium text-gray-900">
+                              {productItem.name}
+                            </h3>
+                            <p className="mt-1 text-sm text-gray-800">
+                              ${productItem.price}{" "}
+                              <span className="text-red-700">{`(-${productItem.priceDrop}%) Off`}</span>
+                            </p>
+                          </div>
+                        </li>
+                      ))
+                  : null}
+              </ul>
+            </div>
+          </div>
+          <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
+            <img
+              src="https://images.unsplash.com/photo-1483985988355-763728e1935b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+              alt="Explore Shop Collection"
+            />
+          </div>
+        </div>
+        <div className="max-w-screen-xl px-4 py-8 mx-auto sm:px-6 sm:py-12 lg:px-8">
+          <div className="text-center">
+            <h2 className="text-xl font-bold text-gray-950 sm:text-3xl">
+              SHOP BY CATEGORY
+            </h2>
+          </div>
+          <ul className="grid grid-cols-1 gap-4 mt-8 lg:grid-cols-3">
+            <li>
+              <div className="relative block group">
+                <img
+                  // src="https://images.unsplash.com/photo-1618898909019-010e4e234c55?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+                  src="https://images.unsplash.com/photo-1620799139834-6b8f844fbe61?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHRzaGlydHN8ZW58MHx8MHx8fDA%3D"
+                  className="object-cover w-full aspect-square"
+                />
+                <div className="absolute inset-0 flex flex-col items-start justify-end p-6">
+                  <h3 className="text-xl font-medium text-white">T-SHIRTS</h3>
+                  <button
+                    onClick={() => router.push("/product/listing/t-shirts")}
+                    className="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white"
+                  >
+                    Shop Now
+                  </button>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="relative block group">
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcD22zJWpm8FThK60-nkBpFB5j2e_RkJdijg&usqp=CAU"
+                  className="object-cover w-full aspect-square"
+                />
+                <div className="absolute inset-0 flex flex-col items-start justify-end p-6">
+                  <h3 className="text-xl font-medium text-white">SHIRTS</h3>
+                  <button
+                    onClick={() => router.push("/product/listing/shirts")}
+                    className="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white"
+                  >
+                    Shop Now
+                  </button>
+                </div>
+              </div>
+            </li>
+            <li className="lg:col-span-2 lg:col-start-2 lg:row-span-2 lg:row-start-1">
+              <div className="relative block group">
+                <img
+                  src="https://images.unsplash.com/photo-1593795899768-947c4929449d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80"
+                  className="object-cover w-full aspect-square"
+                />
+                <div className="absolute inset-0 flex flex-col items-start justify-end p-6">
+                  <h3 className="text-xl font-medium text-white">STORE</h3>
+                  <button
+                    onClick={() => router.push("/product/listing/all-products")}
+                    className="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white"
+                  >
+                    Explore Shop Collection
+                  </button>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </main>
+  );
+}
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,202 @@
+"use client";
+
+import { GlobalContext } from "@/context";
+import { adminNavOptions, navOptions } from "@/utils";
+import { Fragment, useContext, useEffect } from "react";
+import CommonModal from "../CommonModal";
+import Cookies from "js-cookie";
+import { usePathname, useRouter } from "next/navigation";
+import CartModal from "../CartModal";
+import { RiAccountCircleFill, RiAdminFill, RiLogoutCircleRLine, RiLoginCircleLine } from "react-icons/ri";
+import { BsFillCartPlusFill } from "react-icons/bs";
+import { FaUserGroup } from "react-icons/fa6";
+
+function NavItems({ isModalView = false, isAdminView, router }) {
+  return (
+    <div
+      className={`items-center justify-between w-full md:flex md:w-auto ${
+        isModalView ? "" : "hidden"
+      }`}
+      id="nav-items"
+    >
+      <ul
+        className={`flex flex-col p-4 md:p-0 mt-4 font-medium  rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 bg-white ${
+          isModalView ? "border-none" : "border border-gray-100"
+        }`} style={{ backgroundColor: "#73c6d9" }}
+      >
+        {isAdminView
+          ? adminNavOptions.map((item) => (
+              <li
+                className="cursor-pointer block py-2 pl-3 pr-4 text-gray-900 rounded md:p-0"
+                key={item.id}
+                onClick={() => router.push(item.path)}
+              >
+                {item.label}
+              </li>
+            ))
+          : navOptions.map((item) => (
+              <li
+                className="cursor-pointer block py-2 pl-3 pr-4 text-gray-900 rounded md:p-0"
+                key={item.id}
+                onClick={() => router.push(item.path)}
+              >
+                {item.label}
+              </li>
+            ))}
+      </ul>
+    </div>
+  );
+}
+
+export default function Navbar() {
+  const { showNavModal, setShowNavModal } = useContext(GlobalContext);
+  const {
+    user,
+    isAuthUser,
+    setIsAuthUser,
+    setUser,
+    currentUpdatedProduct,
+    setCurrentUpdatedProduct,
+    showCartModal,
+    setShowCartModal
+  } = useContext(GlobalContext);
+
+  const pathName = usePathname();
+  const router = useRouter();
+
+  console.log(currentUpdatedProduct, "navbar");
+
+  useEffect(() => {
+    if (
+      pathName !== "/admin-view/add-product" &&
+      currentUpdatedProduct !== null
+    )
+      setCurrentUpdatedProduct(null);
+  }, [pathName]);
+
+  function handleLogout() {
+    setIsAuthUser(false);
+    setUser(null);
+    Cookies.remove("token");
+    localStorage.clear();
+    router.push("/");
+  }
+
+  const isAdminView = pathName.includes("admin-view");
+
+  return (
+    <>
+      <nav className="bg-white fixed w-full z-20 top-0 left-0 border-b border-gray-200" style={{ backgroundColor: "#73c6d9" }}>
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <div
+            onClick={() => router.push("/")}
+            className="flex items-center cursor-pointer"
+          >
+            <span className="slef-center text-2xl font-semibold whitespace-nowrap">
+              2W0 MEMBERS
+            </span>
+          </div>
+          <div className="flex md:order-2 gap-2">
+            {!isAdminView && isAuthUser ? (
+              <Fragment>
+                <button
+                  className={
+                    "inline-block px-5 py-3 text-xs font-medium upprcase tracking-wide text-red-500"
+                  }
+                  onClick={()=>router.push('/account')}
+                >
+                <RiAccountCircleFill size={25} title="Account"/>
+                </button>
+                <button
+                  className={
+                    "inline-block px-5 py-3 text-xs font-medium upprcase tracking-wide text-red-500"
+                  }
+                  onClick={()=> setShowCartModal(true)}
+                >
+                <BsFillCartPlusFill size={25} title="Cart"/>
+                </button>
+              </Fragment>
+            ) : null}
+            {user?.role === "admin" ? (
+              isAdminView ? (
+                <button
+                  className={
+                    "inline-block px-5 py-3 text-xs font-medium upprcase tracking-wide text-red-500"
+                  }
+                  onClick={() => router.push("/")}
+                >
+                <FaUserGroup size={25} title="Client View"/>
+                </button>
+              ) : (
+                <button
+                  onClick={() => router.push("/admin-view")}
+                  className={
+                    "inline-block px-5 py-3 text-xs font-medium upprcase tracking-wide text-red-500"
+                  }
+                >
+                <RiAdminFill size={25} title="Admin View"/>
+                </button>
+              )
+            ) : null}
+            {isAuthUser ? (
+              <button
+                onClick={handleLogout}
+                className={
+                  "inline-block px-5 py-3 text-xs font-medium upprcase tracking-wide text-red-500"
+                }
+              >
+              <RiLogoutCircleRLine size={25} title="Logout"/>
+              </button>
+            ) : (
+              <button
+                onClick={() => router.push("/login")}
+                className={
+                  "inline-block px-5 py-3 text-xs font-medium upprcase tracking-wide text-red-500"
+                }
+              >
+              <RiLoginCircleLine size={25} title="Login"/>
+              </button>
+            )}
+            <button
+              data-collapse-toggle="navbar-sticky"
+              type="button"
+              className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="navbar-sticky"
+              aria-expanded="false"
+              onClick={() => setShowNavModal(true)}
+            >
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className="w-6 h-6"
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </button>
+          </div>
+          <NavItems router={router} isAdminView={isAdminView} />
+        </div>
+      </nav>
+      <CommonModal
+        showModalTitle={false}
+        mainContent={
+          <NavItems
+            router={router}
+            isModalView={true}
+            isAdminView={isAdminView}
+          />
+        }
+        show={showNavModal}
+        setShow={setShowNavModal}
+      />
+      {showCartModal && <CartModal />}
+    </>
+  );
+}
